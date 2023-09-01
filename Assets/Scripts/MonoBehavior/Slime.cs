f83f9ad1e907b1e5982201eb6b71fa66c4f38f29
Licantropo5 <97682545+Licantropo5@users.noreply.github.com>
2023-09-01 19:58:00 +0200

    some fix to the slime code

--- Assets/Scripts/MonoBehavior/Slime.cs
+++ Assets/Scripts/MonoBehavior/Slime.cs
@@ -2,17 +2,18 @@
 
 namespace GameJam {
 
-	public class Slime : Mob{
-
+	public class Slime : Mob {
+		
 		private void FixedUpdate() {
 			Move();
 		}
 
 		private void Move() {
+			//this can be a strange behavior we want some monster to have
 			if (checkPoint != default) {
-				var position = transform.position;
+				Vector3 position = transform.position;
 				position += new Vector3(checkPoint.x - position.x, checkPoint.y - position.y, 0) * (speed * Time.fixedDeltaTime);
-				transform.position = position;
+				transform.position = position;	
 			}
 		}
 	}
